e0d63caa32b2a4f3f1b743eccb388a9adf843346
Stwn25 <msetiawanwibisono@gmail.com>
2025-06-24 21:01:46 +0700

    fix: distinguish each order process

--- lib/models/prosesOrderCustomer.dart
+++ lib/models/prosesOrderCustomer.dart
@@ -1,23 +1,36 @@
 class ProsesOrderCustomerModel {
   final String noResi;
-  final int idOrder;
-  final int idKurir;
+  final int? idOrder;
+  final int? idKurir;
   final String deskripsi;
   final DateTime tanggalProses;
 
   ProsesOrderCustomerModel({
     required this.noResi,
-    required this.idOrder,
-    required this.idKurir,
+    this.idOrder,
+    this.idKurir,
     required this.deskripsi,
     required this.tanggalProses,
   });
 
-  Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJsonOrderAccepted() => {
     'no_resi': noResi,
     'id_order': idOrder,
     'id_kurir': idKurir,
     'deskripsi': deskripsi,
     'waktu': tanggalProses.toIso8601String(),
+  };
+
+  Map<String, dynamic> toJsonOrderProcessed() => {
+    'no_resi': noResi,
+    'deskripsi': deskripsi,
+    'waktu': tanggalProses.toIso8601String(),
+  };
+
+  Map<String, dynamic> toJsonOrderSendKecamatanPenerima() => {
+    'no_resi': noResi,
+    'deskripsi': deskripsi,
+    'waktu': tanggalProses.toIso8601String(),
+    'id_kurir': idKurir,
   };
 }
